ed249fc11cb4a2c662b7370dd66a602aaceff113
lucas3322 <lucass706@hotmail.com>
2021-03-03 10:13:14 -0300

    mudança da imagem

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,9 +3,9 @@ import styles from '../styles/components/Profile.module.css';
 export function Profile () {
     return(
         <div className={styles.profileContainer}>
-            <img src="https://github.com/diego3.png" alt="Diego Fernandes" />
+            <img src="https://pbs.twimg.com/profile_images/1041636002434084865/JDgOTCng_400x400.jpg" alt="Lucas" />
             <div>
-                <strong>Diego Fernandes</strong>
+                <strong>O brabo tem nome</strong>
                 <p>
                     <img src="icons/level.svg" alt="Level" />
                     level 1
